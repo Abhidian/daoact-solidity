b96655b841ea097b5fe8659924e7a0f480d90593
furusiyya <billal.ariif@gmail.com>
2017-07-16 04:18:26 +0500

    todo: PreICO testing

--- contracts/MultiSig.sol
+++ contracts/MultiSig.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.4.13;
 
 import "SafeMath.sol";
-
 contract MultiSig{
   
   using SafeMath for uint256;
@@ -26,53 +25,72 @@ contract MultiSig{
   event Transfer(address fristSigner, address secondSigner, address thirdSigner, address to,uint256 eth);
   
   // Confirmation done for a transaction.
-  event Confirmations(string action,address signer, bool success,uint256 required);
+  event Confirmed(string action,address signer, uint256 remaining);
+  
+  // 
+  event Voilation(string action, address sender); 
   
   // Administrator key updated
   event KeyReplaced(address oldKey,address newKey);
   
-  function MultiSig(address[4] admins){
-    
-    administrators[admins[0]] = true;
-    administrators[admins[1]] = true;
-    administrators[admins[2]] = true;
-    administrators[admins[3]] = true;
+  function MultiSig(){
+      
+    administrators[0xca35b7d915458ef540ade6068dfe2f44e8fa733c] = true;
+    administrators[0x14723a09acff6d2a60dcdf7aa4aff308fddc160c] = true;
+    administrators[0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db] = true;
+    administrators[0x583031d1113ad414f02576bd6afabfb302140225] = true;
 
   }
   
 
-  function transfer(address _to, uint256 ethers){
+  function transfer(address _to, uint256 ethers) external onlyAdmin {
     
-    require(isAdministrator(msg.sender));
     require( _to != 0x00 );
     require( ethers > 0 );
+    require( ethers >= this.balance);
     
-    uint256 remaining = required.sub(pending.confirmations);
+    uint256 remaining;
     
     if(pending.confirmations == 0){
         
-        pending.signer[pending.confirmations] = address(msg.sender);
+        pending.signer[pending.confirmations] = msg.sender;
         pending.eth = ethers;
         pending.confirmations = pending.confirmations.add(1);
-        Confirmations("Fund Transfer",msg.sender,remaining);
+        remaining = required.sub(pending.confirmations);
+        Confirmed("Fund Transfer",msg.sender,remaining);
         return;
     
     }
         
     if( pending.eth != ethers){
-        nullify();
+        Voilation("Funds Transfer",msg.sender);
+        delete pending;
         return;
     }
     
-    pending.signer[pending.confirmations] = address(msg.sender);
+    if(msg.sender == pending.signer[0]){
+        Voilation("Funds Transfer",msg.sender);
+        delete pending;
+        return;
+    }
+    
+    if( remaining == 1){
+        if(msg.sender != pending.signer[1]){
+            Voilation("Funds Transfer",msg.sender);
+            delete pending;
+            return;
+        }
+    }
+  
+    pending.signer[pending.confirmations] = msg.sender;
     pending.confirmations = pending.confirmations.add(1);
-    Confirmations("Fund Transfer",msg.sender,remaining);
+    Confirmed("Fund Transfer",msg.sender,remaining);
     
     if (pending.confirmations == 3){
         if(_to.send(ethers)){
             Transfer(pending.signer[0],pending.signer[1], pending.signer[2], _to,ethers);
         }
-        nullify();
+        delete pending;
     }
     
   }
@@ -80,10 +98,8 @@ contract MultiSig{
   /**
    * @dev Reset values of pending (Transaction object)
    */
-  function nullify() private{
-       pending.confirmations = 0;
-       delete(pending.signers);
-       pending.eth  = 0;
+  function abortTransaction() external onlyAdmin{
+       delete pending;
   }
   
   
@@ -107,6 +123,7 @@ contract MultiSig{
 
   
   struct KeyUpdate{
+    address[3] signer;
     uint confirmations;
     address oldAddress;
     address newAddress;
@@ -118,41 +135,76 @@ contract MultiSig{
    * @dev
    * @param 
    */
-  function updateAdministratorKey(address _oldAddress, address _newAddress){
+  function updateAdministratorKey(address _oldAddress, address _newAddress) external onlyAdmin {
     require(isAdministrator(msg.sender));
     require(isAdministrator(_oldAddress));
     require( _newAddress != 0x00 );
     require(!isAdministrator(_newAddress));
     require( msg.sender != _oldAddress );
     
+    uint256 remaining;
+    
     if( updating.confirmations == 0){
         
+        updating.signer[updating.confirmations] = msg.sender;
         updating.oldAddress = _oldAddress;
         updating.newAddress = _newAddress;
         updating.confirmations = updating.confirmations.add(1);
-        Confirmations("Fund Transfer",msg.sender,remaining);
+        remaining = required.sub(updating.confirmations);
+        Confirmed("Administrator key Update",msg.sender,remaining);
         return;
+        
     }
     
     if(updating.oldAddress != _oldAddress){
-        updating = KeyUpdate(0,0x00,0x00);
+        Voilation("Administrator key Update",msg.sender);
+        delete updating;
         return;
     }
     
     if(updating.newAddress != _newAddress){
-        updating = KeyUpdate(0,0x00,0x00);
+        Voilation("Administrator key Update",msg.sender);
+        delete updating;
         return;
     }
     
+    if(msg.sender == updating.signer[0]){
+        Voilation("Funds Transfer",msg.sender);
+        delete updating;
+        return;
+    }
+    
+    if( remaining == 1){
+        if(msg.sender != updating.signer[1]){
+            Voilation("Funds Transfer",msg.sender);
+            delete updating;
+            return;
+        }
+    }
+    
+    updating.signer[updating.confirmations] = msg.sender;
     updating.confirmations = updating.confirmations.add(1);
+    Confirmed("Administrator key Update",msg.sender,remaining);
     
     if( updating.confirmations == 3 ){
         KeyReplaced(_oldAddress, _newAddress);
-        updating = KeyUpdate(0,0x00,0x00);
+        delete updating;
         delete administrators[address(_oldAddress)];
         administrators[_newAddress] = true;
-        
         return;
     }
   }
+
+  function abortUpdate() external onlyAdmin{
+      delete updating;
+  }
+    
+
+  modifier onlyAdmin(){
+      if( !administrators[msg.sender] ){
+          revert();
+      }
+      _;
+  }
+  
 }
