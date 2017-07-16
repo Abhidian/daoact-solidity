57a1410b6f1a8e2b789659cf58e6f8c77903f18d
furusiyya <billal.ariif@gmail.com>
2017-07-16 15:31:26 +0500

    PreICO contract for holding funds

--- contracts/PreICO.sol
+++ contracts/PreICO.sol
@@ -1,16 +1,29 @@
 pragma solidity ^0.4.13;
 
 import "SafeMath.sol";
-contract MultiSig{
+
+/**
+ * PreICO is designed to hold funds of pre ico. Account is controlled by four administratos. To trigger a payout
+ * three out of four administrators will must agree on same amount of ethers to be transferred. During the signing
+ * process if one administrator sends different targetted address or amount of ethers, process will abort and they
+ * need to start again.
+ * Administrator can be replaced but three out of four must agree upon replacement of fourth administrator. Three
+ * admins will send address of fourth administrator along with address of new one administrator. If a single one
+ * sends different address the updating process will abort and they need to start again. 
+ */
+
+contract PreICO{
   
   using SafeMath for uint256;
   
+  // Maintain state funds transfer signing process
   struct Transaction{
     address[3] signer;
     uint confirmations;
     uint256 eth;
   }
   
+  // count and record signers with ethers they agree to transfer
   Transaction private pending;
     
   // the number of administrator that must confirm the same operation before it is run.
@@ -24,33 +37,42 @@ contract MultiSig{
   // Funds transfer to other contract
   event Transfer(address fristSigner, address secondSigner, address thirdSigner, address to,uint256 eth);
   
-  // Confirmation done for a transaction.
+  // Administrator successfully signs a transaction
   event Confirmed(string action,address signer, uint256 remaining);
   
-  // 
+  // Administrator voilated consensus
   event Voilation(string action, address sender); 
   
-  // Administrator key updated
+  // Administrator key updated (administrator replaced)
   event KeyReplaced(address oldKey,address newKey);
   
-  function MultiSig(){
-      
-    administrators[0xca35b7d915458ef540ade6068dfe2f44e8fa733c] = true;
-    administrators[0x14723a09acff6d2a60dcdf7aa4aff308fddc160c] = true;
-    administrators[0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db] = true;
-    administrators[0x583031d1113ad414f02576bd6afabfb302140225] = true;
+  
+  function PreICO(address[4] admins){
+    administrators[admins[0]] = true;
+    administrators[admins[1]] = true;
+    administrators[admins[2]] = true;
+    administrators[admins[3]] = true;
 
   }
   
-
+  /**
+   * @dev  To trigger payout three out of four administrators call this
+   * function, funds will be transferred right after verification of
+   * third signer call.
+   * @param _to The address of recipient
+   * @param ethers Amount of ethers to be transferred
+   */
   function transfer(address _to, uint256 ethers) external onlyAdmin {
     
+    // input validations
     require( _to != 0x00 );
     require( ethers > 0 );
     require( ethers >= this.balance);
     
+    // verifications remaining
     uint256 remaining;
     
+    // Start of signing process, first signer will finalize inputs for remaining two
     if(pending.confirmations == 0){
         
         pending.signer[pending.confirmations] = msg.sender;
@@ -61,13 +83,15 @@ contract MultiSig{
         return;
     
     }
-        
+    
+    // Compare amount of ethers with previous confirmtaion
     if( pending.eth != ethers){
         Voilation("Funds Transfer",msg.sender);
-        delete pending;
+        delete pending;  // abort signing process
         return;
     }
     
+    // make sure admin is not trying to spam
     if(msg.sender == pending.signer[0]){
         Voilation("Funds Transfer",msg.sender);
         delete pending;
@@ -86,6 +110,7 @@ contract MultiSig{
     pending.confirmations = pending.confirmations.add(1);
     Confirmed("Fund Transfer",msg.sender,remaining);
     
+    // If three confirmation are done, trigger payout
     if (pending.confirmations == 3){
         if(_to.send(ethers)){
             Transfer(pending.signer[0],pending.signer[1], pending.signer[2], _to,ethers);
@@ -117,11 +142,12 @@ contract MultiSig{
    * @param _addr address The address which you want to check.
    * @return True if the address is an administrator and fase otherwise.
    */
-  function isAdministrator(address _addr) constant returns (bool) {
+  function isAdministrator(address _addr) public constant returns (bool) {
     return administrators[_addr];
   }
 
   
+  // Maintian state of administrator key update process
   struct KeyUpdate{
     address[3] signer;
     uint confirmations;
@@ -132,18 +158,23 @@ contract MultiSig{
   KeyUpdate private updating;
   
   /**
-   * @dev
-   * @param 
+   * @dev Three admnistrator can replace key of fourth administrator. 
+   * @param _oldAddress Address of adminisrator needs to be replaced
+   * @param _newAddress Address of new administrator
    */
   function updateAdministratorKey(address _oldAddress, address _newAddress) external onlyAdmin {
-    require(isAdministrator(msg.sender));
+    
+    // input verifications
     require(isAdministrator(_oldAddress));
     require( _newAddress != 0x00 );
     require(!isAdministrator(_newAddress));
     require( msg.sender != _oldAddress );
     
+    // count confirmation 
     uint256 remaining;
     
+    // start of updating process, first signer will finalize address to be replaced
+    // and new address to be registered, remaining two must confirm
     if( updating.confirmations == 0){
         
         updating.signer[updating.confirmations] = msg.sender;
@@ -156,6 +187,7 @@ contract MultiSig{
         
     }
     
+    // voilated consensus
     if(updating.oldAddress != _oldAddress){
         Voilation("Administrator key Update",msg.sender);
         delete updating;
@@ -168,6 +200,7 @@ contract MultiSig{
         return;
     }
     
+    // make sure admin is not trying to spam
     if(msg.sender == updating.signer[0]){
         Voilation("Funds Transfer",msg.sender);
         delete updating;
@@ -186,20 +219,26 @@ contract MultiSig{
     updating.confirmations = updating.confirmations.add(1);
     Confirmed("Administrator key Update",msg.sender,remaining);
     
+    // if three confirmation are done, register new admin and remove old one
     if( updating.confirmations == 3 ){
         KeyReplaced(_oldAddress, _newAddress);
         delete updating;
-        delete administrators[address(_oldAddress)];
+        delete administrators[_oldAddress];
         administrators[_newAddress] = true;
         return;
     }
   }
 
+  /**
+   * @dev Reset values of updating (KeyUpdate object)
+   */
   function abortUpdate() external onlyAdmin{
       delete updating;
   }
-    
-
+  
+  /**
+   * @dev modifier allow only if function is called by administrator
+   */
   modifier onlyAdmin(){
       if( !administrators[msg.sender] ){
           revert();
