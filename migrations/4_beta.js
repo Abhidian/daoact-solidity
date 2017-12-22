ee0b0007ae993f121153d27a72ebc197c74ae301
Bogdan <bogdan.dmytrenko@draglet.com>
2017-12-22 12:44:42 +0200

    Migrations new version

--- migrations/4_beta.js
+++ migrations/4_beta.js
@@ -1,48 +1,72 @@
-var ProposalController = artifacts.require('./ProposalController.sol');
-var Pool = artifacts.require('./Pool.sol');
-var Curator = artifacts.require('./Curator.sol');
-var ReputationGroup = artifacts.require('./ReputationGroup.sol');
-var Quorum = artifacts.require('./Quorum.sol');
-var CE7mock = artifacts.require('./CE7mock.sol');
-var Vote = artifacts.require('./Vote.sol');
+let ProposalController = artifacts.require('./ProposalController.sol');
+let Pool = artifacts.require('./Pool.sol');
+let Curator = artifacts.require('./Curator.sol');
+let ReputationGroup = artifacts.require('./ReputationGroupDividing.sol');
+let Quorum = artifacts.require('./Quorum.sol');
+let CE7mock = artifacts.require('./CE7mock.sol');
+let Vote = artifacts.require('./Vote.sol');
 
 //migrate -f 4 to run only this migration
 module.exports = function(deployer) {
 
-    var foundationAddress = 0x01;
-    var daoactLTDAddress = 0x01;
-
-    CE7mock.new().then(function(ce7Contract) {
-        console.log('CE7 contract address: ', ce7Contract.address);
-
-        ProposalController.new().then(function(proposalController) {
-            //SET vote contract address MANUALY!
-            console.log('Proposal controller contract address: ', proposalController.address);
-            
-            Quorum.new().then(function(quorumContract) {
-                //SET pool contract address MANUALY!
-                console.log('Quorum contract address: ', quorumContract.address);
-
-                ReputationGroup.new().then(function(reputationContract) {
-                    //SET Curator contract address MANUALY!
-                    console.log('Reputation contract address: ', reputationContract.address);
-
-                    Vote.new(quorumContract.address, proposalController.address, 65000).then(function(voteContract) {
-                        //last argument is ETH price in USD, multiplied by 100. It should be uint value!
-                        console.log('Vote contract address: ', voteContract.address);
-
-                        Pool.new(proposalController.address, voteContract.address, quorumContract.address, foundationAddress, daoactLTDAddress). then(function(poolContract) {
-                            //SET Curator contract address MANUALY!
-                            console.log('Pool contract address: ', poolContract.address);
-
-                            Curator.new(proposalController.address, ce7Contract.address, reputationContract.address, poolContract.address).then(function(curatorContract) {
-                                console.log('Curator contract address: ', curatorContract.address);
-                                console.log('ALL CONTRACT DEPLOYED! Set required addresses!');
-                            });
-                        });
-                    });
-                });
-            });
-        });
-    });
+    const ce7 = async () => {
+        try {
+            return CE7mock.new();
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    const propControl = async () => {
+        try {
+            return ProposalController.new();        
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    const quorum = async () => {
+        try {
+            return Quorum.new();
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    const reputation = async () => {
+        try {
+            return ReputationGroup.new();
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    const vote = async etherPrice => {
+        try {
+            return Vote.new(etherPrice);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    const pool = async (proposal, vote, curator, foundation, dao) => {
+        try {
+            return Pool.new(proposal, vote, curator, foundation, dao);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    const curator = async (token, reputation) => {
+       try {
+        return Curator.new();
+       } catch (err) {
+           console.log(err);
+       }
+    };
+    const migrate = async () => {
+       try {
+        let token = await ce7();
+        console.log(token.address);
+        let result = await curator();
+        console.log('Result address: ', result.address);
+       } catch (err) {
+           console.log(err);
+       }
+    };
+    migrate();
 };
