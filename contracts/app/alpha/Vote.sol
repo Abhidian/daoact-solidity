07b1efd902e2739714d7248ccb409f50037f9a26
ratushnaya <ratushnaya@list.ru>
2018-01-02 17:50:29 +0200

    Vote contract changed

--- contracts/app/alpha/Vote.sol
+++ contracts/app/alpha/Vote.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.18;
+pragma solidity 0.4.18;
 
 import '../../misc/SafeMath.sol';
 import '../../misc/Ownable.sol';
@@ -7,18 +7,20 @@ contract Pool {
     function votesFunding() external payable;
 }
 
-contract Vote is Ownable {
+contract Vote is Ownable{
 
     using SafeMath for uint256;
 
-    address public quorum;
-    address public proposalController;
+    address private quorum;
+
+    address private proposalController;
 
     // sum of all funds that were used to buy ACT_VOTE tokens
-    uint256 public fundsGiven;
+    uint256 private fundsGiven;
 
     // number of votes per ether
-    uint256 public exchangeRate;
+    uint private ethPrice;
+    uint private votePrice;
 
     mapping(address => uint256) private balances;
 
@@ -28,12 +30,10 @@ contract Vote is Ownable {
     event ACTVoteSpent(address indexed voter,uint256 votes);
     event ACTVoteReturned(address indexed voter,uint256 votes);
 
-    function Vote(uint256 _exchangeRate) public {
+    function Vote(){
         owner = msg.sender;
-        require(owner != address(0));
-        require(_exchangeRate > 0);
-
-        exchangeRate = _exchangeRate; //exchange should be uint value in 65000 format. 65000 means 650.00 USD.
+        ethPrice = 850 * 100;
+        votePrice = 1 ether / ethPrice;
     }
 
     modifier onlyProposalController() {
@@ -50,14 +50,13 @@ contract Vote is Ownable {
         require(_newAddress != address(0));
         proposalController = _newAddress;
     }
-
     /**
      * Used for transfering votes from one account to another
      * @param _to Recepient adress
      * @param _value Number of votes to be transferred
      * @return bool True if transferred successfully
      */
-    function transfer(address _to, uint256 _value) public returns (bool) {
+    function transfer(address _to, uint256 _value)returns (bool){
         balances[msg.sender] = balances[msg.sender].sub(_value);
         balances[_to] = balances[_to].add(_value);
         ACTVoteTransfer(msg.sender, _to, _value);
@@ -67,15 +66,14 @@ contract Vote is Ownable {
     /**
      * Receive ethers and load ACT Votes to the account of sender
      */
-    function buyVotes() external payable {
-        uint votes = msg.value.mul(exchangeRate);
-        require(votes == 10);
-
+    function buyVotes() external payable{
+        uint fromMsg = msg.value.div(votePrice);
+        uint votes = fromMsg.div(100);
         balances[msg.sender] = balances[msg.sender].add(votes);
         fundsGiven = fundsGiven.add(msg.value);
-        if (quorum.send(msg.value)) {
+        if(quorum.send(msg.value)){
             ACTVotePurchase(msg.sender,msg.value,votes);
-        } else {
+        }else{
             revert();
         }
     }
@@ -84,7 +82,7 @@ contract Vote is Ownable {
      * Return sum of funds that were used to buy ACT_VOTE tokens
      * @return Funds
      */
-    function funds() view external returns (uint256) {
+    function funds() constant external returns (uint256){
         return fundsGiven;
     }
 
@@ -92,18 +90,26 @@ contract Vote is Ownable {
      * Return ACT_VOTE price
      * @return price
      */
-    function getVotePrice() constant external returns (uint256) {
-        return exchangeRate;
+    function getEthPrice() constant external returns (uint256){
+        return ethPrice;
     }
 
     /**
      * Used to update ACT_VOTE exchange rate with respect to ethers
-     * @param _value new exchange rate
+     * @param _value new exchange rate should be with 2 decimals and multiplied by 100
      * @return true if successful
      */
-    function setPrice(uint256 _value) external onlyOwner returns (bool) {
-        exchangeRate = _value;
+    function setEthPrice(uint256 _value)  external onlyOwner  returns (bool){
+        ethPrice = _value;
         return true;
+    }
+
+    /**
+     * Returns Address of Quorum Platform Contract
+     * @return address
+     */
+    function quorumAddress() constant external returns (address){
+        return quorum;
     }
 
     /**
@@ -111,7 +117,7 @@ contract Vote is Ownable {
      * @param _newAddress updated contract address
      * @return true if successful
      */
-    function updateQuorumAddress(address _newAddress) external onlyOwner returns (bool) {
+    function updateQuorumAddress(address _newAddress)  external onlyOwner returns (bool){
         quorum = _newAddress;
         return true;
     }
@@ -126,32 +132,37 @@ contract Vote is Ownable {
         return true;
     }
 
+
     /**
      * Return ACT_Vote balance of sender
      * @return amount of ACT_VOTE
      */
-    function balanceOf(address _who) view external returns (uint256) {
-        return balances[_who];
+    function balanceOf(address _owner) constant external returns (uint256){
+        return balances[_owner];
     }
 
     // Interface function
-    function isContract() pure external returns (bool) {
+    function isContract() external returns (bool){
         return true;
     }
 
-    function deposit(address _to, uint256 _value) external onlyProposalController returns(bool) {
-        require(_to != address(0));
+    function deposit(address _to,uint256 _value) onlyProposalController external  returns(bool){
+        require(_to != 0x00);
         require(_value > 0);
         balances[_to] = balances[_to].add(_value);
         ACTVoteReturned(_to, _value);
         return true;
     }
 
-    function withdraw(address _from) external onlyProposalController returns(bool) {
-        require(_from != address(0));
-        require(balances[_from] > 0);
-        balances[_from] = balances[_from].sub(1);
-        ACTVoteSpent(_from, 1);
+    function withdraw(address _from,uint256 _value) external onlyProposalController returns(bool){
+        require(_from != 0x00);
+        require(_value > 0);
+        require(balances[_from] >= _value);
+        balances[_from] = balances[_from].sub(_value);
+        ACTVoteSpent(_from,_value);
         return true;
     }
+
+
+
 }
--- contracts/app/prototype/ACTVote.sol
+++ contracts/app/prototype/ACTVote.sol
@@ -3,48 +3,46 @@ pragma solidity 0.4.18;
 import '../../misc/SafeMath.sol';
 import '../../misc/Ownable.sol';
 
-contract ACTVote is Ownable{
-    
+contract Vote is Ownable{
+
     using SafeMath for uint256;
-    
+
     address private quorum;
-    
+
     address private proposalContract;
-    
+
     // sum of all funds that were used to buy ACT_VOTE tokens
     uint256 private fundsGiven;
-    
+
     // number of votes per ether
-    uint256 private exchangeRate;
-    
+    uint private ethPrice;
+    uint private votePrice;
+
     mapping(address => uint256) private balances;
-    
-    event ACTVotePurchase(address indexed buyer,uint256 amount, uint256 votes); 
+
+    event ACTVotePurchase(address indexed buyer,uint256 amount, uint256 votes);
     event ACTVoteTransfer(address indexed from,address indexed to,uint256 votes);
-    
+
     event ACTVoteSpent(address indexed voter,uint256 votes);
     event ACTVoteReturned(address indexed voter,uint256 votes);
-    
-    function ACTVote(address _owner, address _quorumAddr, address _proposalAddr, uint256 _exchangeRate) public{
-        require(_owner != address(0));
-        require(_quorumAddr != address(0));
-        require(_proposalAddr != address(0));
+
+    function Vote(){
+        owner = msg.sender;
+        require(owner != address(0));
         require(_exchangeRate > 0);
 
-        owner = _owner;
-        quorum = _quorumAddr;
-        //proposalContract  = _proposalAddr;
         exchangeRate = _exchangeRate;
     }
 
-    /* Remove after review
-    function ACTVote() public {
-        owner = 0xb115997626d0bE91Ee4b17AAd8330eFC7506C009;
-        quorum = 0xab70CC544477F30ef5DCAb0025ffF40928671d8d;
-        // proposalContract  = _proposalContract;
-        exchangeRate = 100;
-    }*/
+    modifier onlyProposalController() {
+        require(msg.sender == proposalController);
+        _;
+    }
 
+    function setQuorumAddress (address _quorumAddr) public onlyOwner {
+        require(_quorumAddr != address(0));
+        quorum = _quorumAddr;
+    }
 
     /**
      * Used for transfering votes from one account to another
@@ -52,7 +50,7 @@ contract ACTVote is Ownable{
      * @param _value Number of votes to be transferred
      * @return bool True if transferred successfully
      */
-    function transfer(address _to, uint256 _value) public returns (bool){
+    function transfer(address _to, uint256 _value)returns (bool){
         balances[msg.sender] = balances[msg.sender].sub(_value);
         balances[_to] = balances[_to].add(_value);
         ACTVoteTransfer(msg.sender, _to, _value);
@@ -63,10 +61,9 @@ contract ACTVote is Ownable{
      * Receive ethers and load ACT Votes to the account of sender
      */
     function buyVotes() external payable{
-        uint votes = msg.value.mul(exchangeRate);
-        require(votes == 10);
-        
-        balances[msg.sender] = balances[msg.sender].add(votes); 
+        uint fromMsg = msg.value.div(votePrice);
+        uint votes = fromMsg.div(100);
+        balances[msg.sender] = balances[msg.sender].add(votes);
         fundsGiven = fundsGiven.add(msg.value);
         if(quorum.send(msg.value)){
             ACTVotePurchase(msg.sender,msg.value,votes);
@@ -74,7 +71,7 @@ contract ACTVote is Ownable{
             revert();
         }
     }
-    
+
     /**
      * Return sum of funds that were used to buy ACT_VOTE tokens
      * @return Funds
@@ -82,25 +79,25 @@ contract ACTVote is Ownable{
     function funds() constant external returns (uint256){
         return fundsGiven;
     }
-    
+
     /**
      * Return ACT_VOTE price
      * @return price
      */
-    function getVotePrice() constant external returns (uint256){
-        return exchangeRate;
+    function getEthPrice() constant external returns (uint256){
+        return ethPrice;
     }
-    
+
     /**
      * Used to update ACT_VOTE exchange rate with respect to ethers
-     * @param _value new exchange rate
+     * @param _value new exchange rate should be with 2 decimals and multiplied by 100
      * @return true if successful
      */
-    function setPrice(uint256 _value)  external onlyOwner  returns (bool){
-        exchangeRate = _value;
+    function setEthPrice(uint256 _value)  external onlyOwner  returns (bool){
+        ethPrice = _value;
         return true;
     }
-    
+
     /**
      * Returns Address of Quorum Platform Contract
      * @return address
@@ -108,49 +105,49 @@ contract ACTVote is Ownable{
     function quorumAddress() constant external returns (address){
         return quorum;
     }
-    
+
     /**
      * Used to update Address of Quorum Platform Contract in case it is updated
-     * @param _newAddress updated contract address 
+     * @param _newAddress updated contract address
      * @return true if successful
      */
     function updateQuorumAddress(address _newAddress)  external onlyOwner returns (bool){
         quorum = _newAddress;
         return true;
     }
-    
-     /**
-     * Returns Address of Proposal Platform Contract
-     * @return address
-     */
+
+    /**
+    * Returns Address of Proposal Platform Contract
+    * @return address
+    */
     function proposalAddress() constant external returns (address){
         return proposalContract;
     }
-    
+
     /**
      * Used to update Address of Proposal Contract in case it is updated
-     * @param _newAddress updated contract address 
+     * @param _newAddress updated contract address
      * @return true if successful
      */
     function updateProposalAddress(address _newAddress)  external onlyOwner returns (bool){
         proposalContract = _newAddress;
         return true;
     }
-    
-    
+
+
     /**
      * Return ACT_Vote balance of sender
      * @return amount of ACT_VOTE
      */
-    function balanceOf() constant external returns (uint256){
-        return balances[msg.sender];
+    function balanceOf(address _owner) constant external returns (uint256){
+        return balances[_owner];
     }
 
     // Interface function
-    function isContract() external pure returns (bool){
+    function isContract() external returns (bool){
         return true;
     }
-    
+
     function deposit(address _to,uint256 _value) onlyProposalCon external  returns(bool){
         require(_to != 0x00);
         require(_value > 0);
@@ -158,19 +155,20 @@ contract ACTVote is Ownable{
         ACTVoteReturned(_to, _value);
         return true;
     }
-    
+
     function withdraw(address _from,uint256 _value) external onlyProposalCon returns(bool){
         require(_from != 0x00);
         require(_value > 0);
         require(balances[_from] >= _value);
         balances[_from] = balances[_from].sub(_value);
-        ACTVoteSpent(_from,_value); 
+        ACTVoteSpent(_from,_value);
         return true;
     }
-    
+
     modifier onlyProposalCon(){
         require(msg.sender == proposalContract);
         _;
     }
-    
+
+}
 }
